b6d5475aac0a2df48eb2a19bbcdaf31e0cce05fc
Erlan0007 <erlankalykov5@gmail.com>
2025-11-12 08:57:55 +0600

    Add files via upload

---
+++ Task9.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task9 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String d = sc.nextLine();
+//        System.out.println(d.length());
+//
+//        String text = sc.nextLine();
+//        char first = text.charAt(0);
+//        char last = text.charAt(text.length()-1);
+//        System.out.println(first +" "+last);
+//
+//        String test ="My name is java";
+//        boolean java = test.contains("jav123a");
+//        System.out.println(java);
+
+
+//        String msg1 = "OMG";
+//        System.out.println(msg1.toLowerCase());
+//        String msg2 = "omg";
+//        System.out.println(msg2.toUpperCase());
+
+//        String str1 = "abracadabra";
+//        String str2 = "abracadabra";
+//        boolean compare1 = str1.equals(str2);
+//        System.out.println(compare1);
+
+
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//System.out.println(a+b);
+//String dd = Integer.toString(a);
+//        System.out.print(dd);
+//String bb = Integer.toString(b);
+//        System.out.print(bb);
+    }}
+
